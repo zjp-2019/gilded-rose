7262d3788545e6b73d151fcb0e35262c6bcfcbfa
Bartosz <26346113+bbasinski@users.noreply.github.com>
2018-11-12 17:53:44 +0100

    Removed foo test

--- test/GildedRoseTest.php
+++ test/GildedRoseTest.php
@@ -2,15 +2,8 @@
 
 require_once 'gilded_rose.php';
 
-class GildedRoseTest extends \PHPUnit\Framework\TestCase {
-
-    function testFoo() {
-        $items = array(new Item("foo", 0, 0));
-        $gildedRose = new GildedRose($items);
-        $gildedRose->update_quality();
-        $this->assertEquals("foo", $items[0]->name);
-    }
-
+class GildedRoseTest extends \PHPUnit\Framework\TestCase
+{
     public function testQualityDegradesTwiceAsFastWhenSellInExpires()
     {
         /** @var Item[] $items */
