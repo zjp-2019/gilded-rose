79f7a993c307ee88aba4d3038546cd36eb08cbac
Bartosz <26346113+bbasinski@users.noreply.github.com>
2018-11-12 16:49:14 +0100

    Git init

---
+++ test/gilded_rose_test.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once '../src/gilded_rose.php';
+
+class GildedRoseTest extends \PHPUnit\Framework\TestCase {
+
+    function testFoo() {
+        $items = array(new Item("foo", 0, 0));
+        $gildedRose = new GildedRose($items);
+        $gildedRose->update_quality();
+        $this->assertEquals("foo", $items[0]->name);
+    }
+}
